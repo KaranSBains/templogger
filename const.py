2b0d7ef343d5ed50566ac3e773fdac3b4b68f8dd
KaranSBains <109728514+KaranSBains@users.noreply.github.com>
2025-06-06 18:55:06 +0100

    added readme

---
+++ const.py
@@ -0,0 +1,10 @@
+{
+  "domain": "CustomTemperatureSensor",
+  "name": "Temperature sensor analysis",
+  "codeowners": ["@KaranBains"],
+  "dependencies": [],
+  "documentation": "https://www.example.com",
+  "integration_type": "hub",
+  "iot_class": "cloud_polling",
+  "requirements": []
+}
